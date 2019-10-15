4c26d357b6d413ef0235bae1011367b95a60440c
naufalahnaf17 <naufalahnaf37@gmail.com>
2019-10-15 21:51:02 +0800

    sq

--- resources/views/siswa/index.blade.php
+++ resources/views/siswa/index.blade.php
@@ -62,10 +62,7 @@
             <td>{{ $a->nim }}</td>
             <td>{{ $a->nama }}</td>
             <td>{{ $a->kode_jur }}</td>
-            <td>
-              <!-- <a class="btn btn-warning" href="{{url('/edit-siswa/edit/'.$a->nim.'')}}">Edit Data</a> -->
-              <!-- <a class="btn btn-danger" href="{{url('/hapus-siswa/hapus/{{ $a->nim }}')}}">Hapus Data</a> -->
-            </td>
+            
           </tr>
           <?php endforeach; ?>
         <!-- Akhir Data -->
